bd25561a46787e30a3dd081fc213e0bc40defbef
komuro.hiraku <komuro.hiraku@classmethod.jp>
2022-08-15 21:16:37 +0900

    - small fix

--- src/authserver.rs
+++ src/authserver.rs
@@ -1,6 +1,6 @@
 use log::{error, debug};
-use std::io::Write;
 use std::io::{self, BufRead};
+use std::io::Write;
 use std::net::TcpListener;
 
 mod path_util {
@@ -9,6 +9,26 @@ mod path_util {
 
     lazy_static! {
         pub static ref PATH_REGEX: Regex = Regex::new(".*?code=(?P<code>.+)&").unwrap();
+    }
+
+    pub fn split_auth_code(first_line: &str) -> Result<&str, &str> {
+        let mut params = first_line.split_whitespace();
+        let method = params.next();
+        let path = params.next();
+
+        match (method, path) {
+            (Some("GET"), Some(path)) => {
+                println!("path: {}", path);
+
+                if let Some(code) = PATH_REGEX.captures(path) {
+                    let code = code.name("code").unwrap().as_str();
+                    Ok(code)
+                } else {
+                    Err("failed capture auth_code")
+                }
+            }
+            _ => Err("Unknown Http Method."),
+        }
     }
 }
 
@@ -21,9 +41,8 @@ impl AuthCodeServer {
         AuthCodeServer { port }
     }
 
-    pub fn receive_auth_code(self) -> Result<String, ()> {
+    pub fn receive_auth_code(self) -> Result<String, String> {
         let server = TcpListener::bind(format!("127.0.0.1:{}", self.port)).unwrap();
-        let mut auth_code_opt: Option<String> = None;
         if let Some(stream) = server.incoming().next() {
             match stream {
                 Ok(stream) => {
@@ -33,47 +52,25 @@ impl AuthCodeServer {
                         error!("{}", error);
                     }
 
-                    // スペースで分割
-                    let mut params = first_line.split_whitespace();
-                    let method = params.next();
-                    let path = params.next();
-                    let auth_code_result = match (method, path) {
-                        (Some("GET"), Some(path)) => {
+                    match path_util::split_auth_code(first_line.as_str()) {
+                        Ok(code) => {
                             let stream = stream.get_mut();
-                            println!("path: {}", path);
+                            writeln!(stream, "HTTP/1.1 200 OK").unwrap();
+                            writeln!(stream, "Content-Type: text/plain; charset=UTF-8").unwrap();
+                            writeln!(stream, "auth_code={}", code.to_string()).unwrap();
 
-                            if let Some(code) = path_util::PATH_REGEX.captures(path) {
-                                let code = code.name("code").unwrap().as_str();
-
-                                // ブラウザに空レスポンス
-                                writeln!(stream, "HTTP/1.1 200 OK").unwrap();
-                                writeln!(stream, "Content-Type: text/plain; charset=UTF-8")
-                                    .unwrap();
-                                writeln!(stream, "{}", code).unwrap();
-                                Ok(code)
-                            } else {
-                                Err("failed capture auth_code")
-                            }
-                        }
-                        _ => Err("unknown Method"),
-                    };
-                    match auth_code_result {
-                        Ok(code) => auth_code_opt = Some(code.to_string()),
-                        Err(str) => error!("{}", str),
+                            debug!("get auth code: {}", code);
+                            return Ok(code.to_string());
+                        },
+                        Err(err) => error!("{}", err),
                     }
                 }
-                Err(_) => {
-                    error!("Callback Server error");
+                Err(err) => {
+                    error!("Server Received Unexpected Response. {}", err);
                 }
             }
         }
-        match auth_code_opt {
-            Some(code) => {
-                debug!("get auth code: {}", code);
-                Ok(code)
-            },
-            None => Err(()),
-        }
+        Err("Can not receive auth_code".to_string())
     }
 }
 
